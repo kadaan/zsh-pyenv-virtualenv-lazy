b8dd4a16d53176f7380e38fced2501c3eb8cbb41
Joel Baranick <joel.baranick@ensighten.com>
2018-06-12 14:35:53 -0700

    Fix the unset command

--- pyenv-virtualenv-lazy.plugin.zsh
+++ pyenv-virtualenv-lazy.plugin.zsh
@@ -1,7 +1,7 @@
 # Lazy load pyenv-virtualenv
 if type pyenv-virtualenv &> /dev/null; then
   function pyenv-virtualenv() {
-    unset pyenv-virtualenv
+    unset -f pyenv-virtualenv > /dev/null 2>&1
     eval "$(command pyenv virtualenv-init -)"
     pyenv-virtualenv "$@"
   }
